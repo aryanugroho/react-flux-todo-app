e0c57f539a87ba71c79deab4616b06136371983b
nshahpazov <nshahpazov@gmail.com>
2015-08-02 13:12:24 +0300

    moved in a main app file

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,7 +1,8 @@
 /// <reference path="../../typing/react.d.ts" />
 import * as React from 'react';
+import ChatApp from './components/ChatApp';
 
  React.render(
-    <ChatApp width={300} height={300} color={"#336699"} />,
+    <ChatApp />,
     document.getElementById('example')
 );
