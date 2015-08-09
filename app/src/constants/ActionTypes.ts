80784dbf3ea65cfd6d85124ed59d8e5afc1a57f0
nshahpazov <nshahpazov@gmail.com>
2015-08-09 14:53:03 +0300

    todo types init

--- app/src/constants/ActionTypes.ts
+++ app/src/constants/ActionTypes.ts
@@ -1,7 +1,7 @@
 enum ActionTypes {
   CHANGE_COLOR_ACTION,
-  INSERT_COLOR_ACTION
-
+  INSERT_COLOR_ACTION,
+  CHECK_TODO_ACTION
 }
 
 export default ActionTypes;
---
+++ app/src/constants/TodoActionTypes.ts
@@ -0,0 +1,7 @@
+enum TodoActionTypes {
+    CREATE_TODO_ACTION,
+    CHANGE_TODO_TEXT_ACTION,
+    CHECK_TODO_ACTION
+}
+
+export default TodoActionTypes;
