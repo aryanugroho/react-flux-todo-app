3a713f43370839378137753c55d89afc3f290ff9
nshahpazov <nshahpazov@gmail.com>
2015-08-09 14:53:36 +0300

    clean now

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import ChatApp from './components/ChatApp';
 
- React.render(
-    <ChatApp />,
-    document.getElementById('example')
+React.render(
+  <ChatApp />,
+  document.getElementById('example')
 );
