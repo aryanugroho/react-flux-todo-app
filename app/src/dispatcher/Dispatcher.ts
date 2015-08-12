1764a4a03fd027c8c5a3f8fb8a54ac9086b1af9b
nshahpazov <nshahpazov@gmail.com>
2015-08-12 14:21:36 +0300

    using fb dispatcher

--- app/src/dispatcher/Dispatcher.ts
+++ app/src/dispatcher/Dispatcher.ts
@@ -1,30 +1,4 @@
-class Dispatcher {
-  _callbacks: {[key: string]: Function};
-  _lastID: number;
-  _isDispatching: boolean;
+/// <reference path="../../../typing/flux.d.ts" />
+import * as Flux from 'flux';
 
-  constructor() {
-    this._callbacks = {};
-    this._lastID = 1;
-  }
-
-  dispatch(payload: any) {
-    Object.keys(this._callbacks).forEach((key) => {
-      this._callbacks[key].call(this, payload);
-    }, this);
-  }
-
-  register(callback: Function) {
-    let _prefix: string = 'ID_';
-    let id: string = _prefix + this._lastID++;
-
-    this._callbacks[id] = callback;
-    return id;
-  }
-
-  unregister(id: string): void {
-    delete this._callbacks[id];
-  }
-}
-
-export default new Dispatcher();
+export default new Flux.Dispatcher();
